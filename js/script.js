71b234a4a84e44f057a875fa82078fd6c426d7ae
Gabriel Lopes <gabriellopes1005@gmail.com>
2021-03-22 21:12:45 -0300

    Js Animations

---
+++ js/script.js
@@ -0,0 +1,15 @@
+if(window.SimpleSlide){
+    new SimpleSlide({
+        slide: 'quote',
+        time: 5000, // tempo de transição dos slides
+    });
+    
+    new SimpleSlide({
+        slide: 'portfolio',
+        time: 5000,
+        nav: true,
+    });    
+}
+if(window.SimpleAnime){
+    new SimpleAnime();
+}
